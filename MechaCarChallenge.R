a287c116bd8f9594496828e24b1c878776fdbd4a
joszpr <85839235+joszpr@users.noreply.github.com>
2021-10-22 17:52:08 -0400

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,44 @@
+# Import libraries
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+
+# Import MechaCar_mpg CSV file as a dataframe
+ds_import <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+# Plot data to visualize variables relationship with mpg
+  # Vehicle_length
+plt1 <- ggplot(ds_import,aes(x=vehicle_length,y=mpg))
+plt1 + geom_point()
+  # vehicle_weight
+plt2 <- ggplot(ds_import,aes(x=vehicle_weight,y=mpg))
+plt2 + geom_point()
+  # spoiler_angle
+plt3 <- ggplot(ds_import,aes(x=spoiler_angle,y=mpg))
+plt3 + geom_point()
+  # ground_clearance
+plt4 <- ggplot(ds_import,aes(x=ground_clearance,y=mpg))
+plt4 + geom_point()
+  # AWD
+plt5 <- ggplot(ds_import,aes(x=AWD,y=mpg))
+plt5 + geom_point()
+
+# Perform linear regression on ds_import
+linear_reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=ds_import)
+summary(linear_reg)
+
+#
+# Suspension_Coil.csv Analysis
+#
+
+# Import the Suspension_coil.csv into a dataframe
+sc_import <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Get the summary statistics of sc_import data
+total_summary <- sc_import %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+# Get the summary statistics of sc_import data separated by Manufacturing_Lot
+lot_summary <- sc_import %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+
+
